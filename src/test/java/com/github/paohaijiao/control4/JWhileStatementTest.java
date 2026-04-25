931a338710ccdbd09ad20c53ead9370e48f17ad0
martin <goudingcheng@gmail.com>
2026-04-25 20:26:31 +0800

    bugfix: whileStatement

---
+++ src/test/java/com/github/paohaijiao/control4/JWhileStatementTest.java
@@ -0,0 +1,63 @@
+package com.github.paohaijiao.control4;
+
+
+import com.github.paohaijiao.param.JContext;
+import com.github.paohaijiao.parser.JQuickJavaLexer;
+import com.github.paohaijiao.parser.JQuickJavaParser;
+import com.github.paohaijiao.support.JQuickJavaTypeReference;
+import com.github.paohaijiao.visitor.JQuickJavaCommonVisitor;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.Stack;
+
+public class JWhileStatementTest {
+    @Test
+    public void while1() throws IOException {
+        String rule="    while(true){\n" +
+                "            continue;\n" +
+                "        }";
+        System.out.println(rule);
+        JQuickJavaLexer lexer = new JQuickJavaLexer(CharStreams.fromString(rule));
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        JQuickJavaParser parser = new JQuickJavaParser(tokens);
+        JQuickJavaParser.WhileStatementContext tree = parser.whileStatement();
+        JQuickJavaCommonVisitor tv = new JQuickJavaCommonVisitor(lexer,tokens,parser);
+        tv.visit(tree);
+    }
+    @Test
+    public void whilebreak() throws IOException {
+        String rule="    while(true){\n" +
+                "            break;\n" +
+                "        }";
+        System.out.println(rule);
+        JQuickJavaLexer lexer = new JQuickJavaLexer(CharStreams.fromString(rule));
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        JQuickJavaParser parser = new JQuickJavaParser(tokens);
+        JQuickJavaParser.WhileStatementContext tree = parser.whileStatement();
+        JQuickJavaCommonVisitor tv = new JQuickJavaCommonVisitor(lexer,tokens,parser);
+        tv.visit(tree);
+    }
+    @Test
+    public void whileif() throws IOException {
+        String rule=" while(true){\n" +
+                "            for (int a = 0; a<10; a=a+1){\n" +
+                "                if(a==2){\n" +
+                "                    continue;\n" +
+                "                }else{\n" +
+                "                    console.log(\"当前的变量a:\"+a);\n" +
+                "                }\n" +
+                "            }\n" +
+                "            break;\n" +
+                "        }";
+        System.out.println(rule);
+        JQuickJavaLexer lexer = new JQuickJavaLexer(CharStreams.fromString(rule));
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        JQuickJavaParser parser = new JQuickJavaParser(tokens);
+        JQuickJavaParser.WhileStatementContext tree = parser.whileStatement();
+        JQuickJavaCommonVisitor tv = new JQuickJavaCommonVisitor(lexer,tokens,parser);
+        tv.visit(tree);
+    }
+}
